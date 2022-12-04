a9c0fd27909d4901ace2c8fb22b35d208326ec52
Shawn Jonghyuck Jung <41564982+shawn-jung@users.noreply.github.com>
2022-12-03 20:45:39 -0800

    Update day01.py

--- day01/day01.py
+++ day01/day01.py
@@ -2,6 +2,7 @@
 with open('puzzle_input.txt') as file_txt:
     lines = [line.rstrip().lstrip() for line in file_txt]
 
+# setting up variables     
 elfno = 0 #counting each elf 
 carries = {} #dict to get sum of fruits per each elf 
 
@@ -24,4 +25,5 @@ elf_fruit = sorted(carries.items(), key = lambda x: x[1], reverse=True)[0][1]
 
 print(f'elf #{elf_no+1} has total calorie of {elf_fruit} in the bag!')
 # Elves do not count from zero! So I will add one. 
+print('the answer is: ', elf_fruit)
 
