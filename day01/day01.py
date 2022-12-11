e60eb92a38399593210465d6e5ff13aacecfb9b8
Shawn Jonghyuck Jung <41564982+shawn-jung@users.noreply.github.com>
2022-12-11 15:34:50 -0800

    adding day2

--- day01/day01.py
+++ day01/day01.py
@@ -1,5 +1,5 @@
 # open the text file
-file_name = 'puzzle_input.txt')
+file_name = 'puzzle_input.txt'
 with open(file_name) as file_txt:
     lines = [line.rstrip().lstrip() for line in file_txt]
 
@@ -16,11 +16,11 @@ for i in range(len(lines)):
 
 
 # wanted to use lambda to get second item, value
-sorted(carries.items(), key = lambda x: x[1], reverse=True)[0:3]
+top3 = sorted(carries.items(), key = lambda x: x[1], reverse=True)[0:3]
 
 # May I can use itemgetter 
 from operator import itemgetter 
-sorted(carries.items(), key = itemgetter(1), reverse=True)[0:3]
+top3 = sorted(carries.items(), key = itemgetter(1), reverse=True)[0:3]
 
 top_elf = sorted(carries.items(), key = lambda x: x[1], reverse=True)[0]
 elf_no = top_elf[0]
@@ -29,4 +29,7 @@ elf_fruit = top_elf[1]
 print(f'elf #{elf_no+1} has total calorie of {elf_fruit} in the bag!')
 # Elves do not count from zero! So I will add one. 
 print('the answer is: ', elf_fruit)
+
+top3_calorie_sum = sum(v for k, v in top3)
+print(f'Top 3 elves carry {top3_calorie_sum} of snacks.')
 
